06833cbc7e8ba2d779b8a59774fead1ef5fe4c2c
Tabassum Hassan <tiram819@gmail.com>
2020-03-09 19:31:10 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,13 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(array)
-   i = 0
-   while i < array.length
-   yield(array[i])
-   i += 1
-  end
- array
- end
+  i = 0 
+  collection = []
+while i < array.length   
+  collection << yield(array[i])
+  i += 1 
+end 
+collection 
+end 
 
  
 
